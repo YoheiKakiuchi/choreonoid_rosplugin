3d88f67f9e94176dd65824df9844120a787fe4d8
Yohei Kakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2019-07-08 13:54:57 +0900

    add stop/start-simulation

--- src/ROSPlugin.cpp
+++ src/ROSPlugin.cpp
@@ -1,51 +1,133 @@
-#include "BodyPublisherItem.h"
-#include <cnoid/Plugin>
-#include <cnoid/MessageView>
-#include <ros/init.h>
-#include <ros/master.h>
-#include <ros/spinner.h>
+#include "ROSPlugin.h"
+
+#include <cnoid/SimulationBar>
+#include <cnoid/ItemTreeView>
+#include <cnoid/RootItem>
+#include <cnoid/Body>
+#include <cnoid/BodyItem>
+
+#include <random>
 
 using namespace std;
 using namespace cnoid;
 
-class ROSPlugin : public Plugin
+ROSPlugin::ROSPlugin() : Plugin("ROS")
 {
-    std::unique_ptr<ros::AsyncSpinner> spinner;
-    
-public:
-    ROSPlugin() : Plugin("ROS") {
-        require("Body");
+  require("Body");
+}
+
+bool ROSPlugin::initialize()
+{
+  std::cerr << "ROSPlugin::initialize" << std::endl;
+  int argc = 0;
+  char** argv = 0;
+  bool primary_plugin = false;
+  if(!ros::isInitialized()){
+    ros::init(argc, argv, "choreonoid", ros::init_options::NoSigintHandler);
+    primary_plugin = true;
+  }
+
+  if(!ros::master::check()){
+    MessageView::instance()->putln(MessageView::WARNING, "The ROS master is not found.");
+    return false;
+  }
+
+  nh = boost::shared_ptr<ros::NodeHandle>(new ros::NodeHandle("choreonoid"));
+
+  std::string reset_simulation_service_name("reset_simulation");
+  ros::AdvertiseServiceOptions reset_simulation_aso =
+    ros::AdvertiseServiceOptions::create<std_srvs::Empty>(reset_simulation_service_name,
+                                                          boost::bind(&ROSPlugin::resetSimulation,this,_1,_2),
+                                                          ros::VoidPtr(), &srv_queue);
+  reset_simulation_service_ = nh->advertiseService(reset_simulation_aso);
+
+  std::string stop_simulation_service_name("stop_simulation");
+  ros::AdvertiseServiceOptions stop_simulation_aso =
+    ros::AdvertiseServiceOptions::create<std_srvs::Empty>(stop_simulation_service_name,
+                                                          boost::bind(&ROSPlugin::stopSimulation,this,_1,_2),
+                                                          ros::VoidPtr(), &srv_queue);
+  stop_simulation_service_ = nh->advertiseService(stop_simulation_aso);
+
+  spinner.reset(new ros::AsyncSpinner(0));
+  spinner->start();
+
+  // spin for srv_queue
+  // timer_callback should be executed by the thread which initialized this plugin
+  spinTimer.sigTimeout().connect([&](){ timer_callback(); });
+  spinTimer.start(1.0);
+
+  BodyPublisherItem::initialize(this);
+
+  //// initialize ros service ...
+  return true;
+}
+
+bool ROSPlugin::finalize()
+{
+  //ros::requestShutdown();
+  //ros::waitForShutdown();
+  if (!!reset_simulation_service_) {
+    ////
+  }
+}
+
+bool ROSPlugin::stopSimulation(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
+{
+  std::cerr << "[ROSPlugin] stopSimulation" << std::endl;
+  //sim = SimulatorItem::findActiveSimulatorItemFor(this))
+  ItemList<SimulatorItem> simulators =
+    ItemTreeView::mainInstance()->selectedItems<SimulatorItem>();
+
+  for(size_t i=0; i < simulators.size(); ++i) {
+    SimulatorItem* simulator = simulators.get(i);
+    simulator->stopSimulation();
+  }
+}
+
+bool ROSPlugin::resetSimulation(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res)
+{
+  std::cerr << "[ROSPlugin] resetSimulation" << std::endl;
+
+  ItemTreeView* itemTreeView = ItemTreeView::instance();
+  ItemList<BodyItem> bodyItems;
+  bodyItems.extractChildItems(RootItem::instance());
+
+  //// search specific body
+  BodyItem* bodyItem;
+  for(size_t i=0; i < bodyItems.size(); ++i){
+    BodyItem* bdI = bodyItems.get(i);
+    //std::cerr << "nm[" << i << "] : " << bdI->name() << std::endl;
+    if (bdI->name() == "InvPendulum") { //// fixed string
+      bodyItem = bdI;
     }
-  
-    virtual bool initialize()
-    {
-        int argc = 0;
-        char** argv = 0;
-        bool primary_plugin = false;
-        if(!ros::isInitialized()){
-            ros::init(argc, argv, "choreonoid", ros::init_options::NoSigintHandler);
-            primary_plugin = true;
-        }
+  }
+  if (!!bodyItem) {
+    bodyItem->restoreInitialState(true);
+    std::random_device rnd;
+    double rand = 0.5 * (rnd() / (double)std::random_device::max()) - 0.25;
+    Link *lk = bodyItem->body()->rootLink();
+    AngleAxis aa(rand ,Vector3d::UnitY());
+    lk->setRotation(aa);
+    Vector3d p(0, rand, 0.1);
+    lk->setTranslation(p);
+    bodyItem->storeInitialState();
+  }
+  ////
 
-        if(!ros::master::check()){
-            MessageView::instance()->putln(
-                MessageView::WARNING, "The ROS master is not found.");
-            return false;
-        }
+  // simulator start
+  ItemList<SimulatorItem> simulators =
+    ItemTreeView::mainInstance()->selectedItems<SimulatorItem>();
+  for(size_t i=0; i < simulators.size(); ++i) {
+    SimulatorItem* simulator = simulators.get(i);
+    simulator->startSimulation(true);
+  }
 
-        spinner.reset(new ros::AsyncSpinner(0));
-        spinner->start();
+  return true;
+}
 
-        BodyPublisherItem::initialize(this);
-        
-        return true;
-    }
-
-    virtual bool finalize()
-    {
-        ros::requestShutdown();
-        ros::waitForShutdown();
-    }
-};
+void ROSPlugin::timer_callback()
+{
+  srv_queue.callAvailable(ros::WallDuration());
+}
 
 CNOID_IMPLEMENT_PLUGIN_ENTRY(ROSPlugin)
---
+++ src/ROSPlugin.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "BodyPublisherItem.h"
+//
+#include <cnoid/Plugin>
+#include <cnoid/MessageView>
+#include <cnoid/Timer>
+
+#include <ros/ros.h>
+#include <ros/callback_queue.h>
+#include <ros/subscribe_options.h>
+#include <rosgraph_msgs/Clock.h>
+#include <ros/spinner.h>
+
+#include <geometry_msgs/Wrench.h>
+#include <std_srvs/Empty.h>
+
+namespace cnoid {
+
+class ROSBar;
+
+class ROSPlugin : public Plugin
+{
+protected:
+  ROSBar* mBar;
+  std::unique_ptr<ros::AsyncSpinner> spinner;
+  boost::shared_ptr<ros::NodeHandle> nh;
+
+  ros::ServiceServer reset_simulation_service_;
+  ros::ServiceServer stop_simulation_service_;
+
+  ros::CallbackQueue srv_queue;
+  Timer spinTimer;
+
+public:
+  ROSPlugin();
+  virtual bool initialize();
+  virtual bool finalize();
+  void timer_callback();
+
+  bool resetSimulation(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+  bool stopSimulation(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res);
+};
+
+}
